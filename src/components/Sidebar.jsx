d74360dcbbed0d21a678a57a1169084f7737653d
frzxyz <accsmurfer@gmail.com>
2024-09-01 22:54:08 +0700

    Update Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,14 @@
-// src/components/Sidebar.js
+import Link from 'next/link';
 
 export default function Sidebar() {
   return (
-    <nav className="col-md-2 d-md-block  sidebar">
+    <nav className="col-md-2 d-md-block sidebar">
       <div className="position-sticky">
-        <h3 className="sidebar-heading px-3 py-2">MovNow</h3>
+        <h3 className="sidebar-heading px-3 py-2">
+          <Link href="/" className="text-dark text-decoration-none">
+            MovNow
+          </Link>
+        </h3>
         <ul className="nav flex-column">
           <li className="nav-item">
             <a className="nav-link active text-dark" href="#">Japan</a>
